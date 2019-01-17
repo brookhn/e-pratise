86a90edf02927a00303e6cb99def0805a965ab0d
yangtao <18111987@suningcultural.com>
2019-01-17 16:29:24 +0800

    goods

---
+++ src/main/java/com/pp/server/EPratiseApplication.java
@@ -0,0 +1,14 @@
+package com.pp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPratiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPratiseApplication.class, args);
+	}
+
+}
+
